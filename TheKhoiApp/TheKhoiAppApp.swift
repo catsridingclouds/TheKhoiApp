bc353e7b38350ee9cb32916735e4f3b5628e452f
anj0ey <cajigal@usc.edu>
2025-11-06 20:07:19 -0800

    Initial Push

---
+++ TheKhoiApp/TheKhoiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheKhoiAppApp.swift
+//  TheKhoiApp
+//
+//  Created by Anjo on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TheKhoiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
